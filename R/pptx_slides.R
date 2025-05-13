63051922b2517a558b3f542c202cc3c1cbd69ff7
naddc <nadia.duffo@pucp.edu.pe>
2025-05-13 17:38:40 -0500

    Actualiza función pptx_slides

--- R/pptx_slides.R
+++ R/pptx_slides.R
@@ -3,6 +3,7 @@
 #' @import officedown
 #' @export
 pptx_slides <- function(...) {
-  officedown::rpptx_document(reference_doc = system.file("rmarkdown/templates/pptx_slides/skeleton/template.pptx", package = "pulso2"),
+
+  officedown::rpptx_document(reference_doc = system.file("rmarkdown/templates/pptx_slides/skeleton/template.pptx", package = "pulsoreport"),
                              slide_level = 2)
 }
