66f5dad672975235e5d424575892580645d0378a
naddc <nadia.duffo@pucp.edu.pe>
2025-05-13 14:41:36 -0500

    Cambio de ruta de imágenes accesorias a ext/

--- R/cargar_datos.R
+++ R/cargar_datos.R
@@ -10,11 +10,11 @@ cargar_datos <- function(params) {
       message(paste("Cargando", nombre, "desde", filepath))
       assign(nombre, haven::read_sav(filepath), envir = .GlobalEnv)
     } else {
-      message(paste("Archivo no encontrado en", filepath, "- Buscando en paquete 'pulso2'..."))
-      filepath_pkg <- system.file(file.path('extdata', params[[nombre]]), package = "pulso2")
+      message(paste("Archivo no encontrado en", filepath, "- Buscando en paquete 'pulsoreport'..."))
+      filepath_pkg <- system.file(file.path('extdata', params[[nombre]]), package = "pulsoreport")
 
       if (file.exists(filepath_pkg) && nzchar(filepath_pkg)) {
-        message(paste("Cargando", nombre, "desde el paquete 'pulso2' en", filepath_pkg))
+        message(paste("Cargando", nombre, "desde el paquete 'pulsoreport' en", filepath_pkg))
         assign(nombre, haven::read_sav(filepath_pkg), envir = .GlobalEnv)
       } else {
         message(paste("Error: Archivo no encontrado ni en local ni en el paquete para", nombre))
