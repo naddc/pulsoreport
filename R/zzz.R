07031fd626d97e6a97b67ce777783af742de3490
Nadia <166047012+naddc@users.noreply.github.com>
2025-06-19 10:24:32 -0500

    Actualización desde nueva computadora

---
+++ R/zzz.R
@@ -0,0 +1,19 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom tibble tribble
+#' @export
+tibble::tribble
+
+#' @importFrom dplyr select filter
+#' @export
+dplyr::select
+#' @export
+dplyr::filter
+
+#' @importFrom officer fp_border ph_location
+#' @export
+officer::fp_border
+#' @export
+officer::ph_location
